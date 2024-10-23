85a47ec98a6c08e60f5a8121f94fd1cc71e09c55
vitorblue007 <161150686+vitorblue007@users.noreply.github.com>
2024-10-22 22:45:35 -0300

    Add files via upload

---
+++ webhookURL.js
@@ -0,0 +1,2 @@
+// webhookURL.js
+module.exports = 'https://discord.com/api/webhooks/1298430533256351767/a_P6fu81C6kV_lLFJ4Zw_zhI2I0uixWVqR817e3yGHgy7GDmwLgnVWeui6SK7tGElvTF'; // Replace with your actual webhook URL
